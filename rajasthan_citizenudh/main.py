096acde3f97a226fa4216820ed3fa1c52981da4d
Sandeep Kumar <sandeepgadhwal1@gmail.com>
2026-06-08 03:17:10 +0900

    doc

--- rajasthan_citizenudh/main.py
+++ rajasthan_citizenudh/main.py
@@ -20,9 +20,9 @@ def get_token(session: requests.Session) -> dict[str, str]:
         tokens[service["ServiceName"]] = service["Token"]
     return tokens
 
-def get_layers_metadata(session: requests.Session, map_servers: list[tuple[str, str]]) -> list[dict]:
+def get_layers_metadata(session: requests.Session, map_servers: list[tuple[str, str, bool]]) -> list[dict]:
     metadata_list = []
-    for mapserver, token in tqdm(map_servers, desc="Preparing metadata for map servers"):
+    for mapserver, token, count_rows in tqdm(map_servers, desc="Preparing metadata for map servers"):
         mapserver_url = f"{SERVICES_URL}/{mapserver}/MapServer"
         params = {
             "f": "json",
@@ -33,23 +33,28 @@ def get_layers_metadata(session: requests.Session, map_servers: list[tuple[str, 
         for layer in mapserver_metadata["layers"]:
             id = layer["id"]
             query_url = f"{mapserver_url}/{id}/query"
-            params = {
-                'where': '1=1',
-                "f": "json",
-                "token": token,
-                "returnCountOnly": "true"
-            }
-            r_layer = session.get(query_url, params=params)
             metadata = {
                 'query_url': query_url,
                 'name': layer["name"],
                 'mapserver': mapserver,
-                'count': r_layer.json()["count"],
                 'max_record_count': mapserver_metadata["maxRecordCount"],
-                "token": token
+                "token": token,
+                'count_rows': count_rows
             }
             metadata_list.append(metadata)
     return metadata_list
+
+
+def get_layer_count(session: requests.Session, query_url: str, token: str) -> int:
+    params = {
+        'where': '1=1',
+        "f": "json",
+        "token": token,
+        "returnCountOnly": "true"
+    }
+    r = session.get(query_url, params=params)
+    return r.json()["count"]
+
 
 def scrape_layer(session: requests.Session, metadata: dict) -> None:
     output_layer_path = OUTPUT_DIR / metadata['mapserver'] / f"{metadata['name']}.gpkg"
@@ -57,11 +62,15 @@ def scrape_layer(session: requests.Session, metadata: dict) -> None:
         print(f"Layer {metadata['name']} already scraped. Skipping.")
         return
 
+    if metadata["count_rows"]:
+        total_count = get_layer_count(session, metadata["query_url"], metadata["token"])
+    else:
+        total_count = int(1e9) # scrape all rows without counting first
     layer_cache_path = CACHE_DIR / metadata['mapserver'] / metadata['name']
     layer_cache_path.mkdir(parents=True, exist_ok=True)
     query_url = metadata["query_url"]
     result_offset = 0
-    max_record_count = min(1000, metadata["max_record_count"])
+    max_record_count = min(10, metadata["max_record_count"])
     params =  {
         'where': '1=1',
         "f": "json",
@@ -72,7 +81,7 @@ def scrape_layer(session: requests.Session, metadata: dict) -> None:
         "outFields": "*"
     }
     df_store = []
-    for result_offset in tqdm(range(0, metadata["count"], max_record_count), desc=f"Scraping layer {metadata['name']}"):
+    for result_offset in tqdm(range(0, total_count, max_record_count), desc=f"Scraping layer {metadata['name']}"):
         params["resultOffset"] = result_offset
         cache_path = layer_cache_path / f"{metadata['name']}_{result_offset}_{result_offset + max_record_count}.json"
         if not cache_path.exists():
@@ -83,6 +92,9 @@ def scrape_layer(session: requests.Session, metadata: dict) -> None:
                 json.dump(json_response, f)
     
         df = gpd.read_file(cache_path)
+        if len(df) == 0:
+            print(f"No more data to scrape for layer {metadata['name']}. Stopping. Total rows scraped: {len(df_store)*max_record_count}")
+            break
         df_store.append(df)
     if len(df_store) == 0:
         print(f"No data scraped for layer {metadata['name']}. Skipping.")
@@ -104,15 +116,14 @@ def main():
     # with open("home.html", "wb") as f:
     #     f.write(r.content)
     tokens = get_token(session)
-
     # Scrape map servers
     map_servers = [
-        ("JDA/CCZM", tokens["geocode"]),
-        ("JDA/InstitutionalAllotment", tokens["geocode"]),
-        ("JDA/MasterDevelopmentPlan", tokens["geocode"]),
-        ("JDA/MasterDevelopmentPlanDraft", tokens["geocode"]),
-        ("JDA/Settlement_JDA", tokens["geocode"]),
-        ("JDA/JDA_Plots", tokens["geocode"]),
+        ("JDA/CCZM", tokens["geocode"], True),
+        ("JDA/InstitutionalAllotment", tokens["geocode"], True),
+        ("JDA/MasterDevelopmentPlan", tokens["geocode"], True),
+        ("JDA/MasterDevelopmentPlanDraft", tokens["geocode"], True),
+        ("JDA/Settlement_JDA", tokens["geocode"], False),
+        ("JDA/JDA_Plots", tokens["geocode"], True),
     ]
     layers_metadata = get_layers_metadata(session, map_servers)
 
